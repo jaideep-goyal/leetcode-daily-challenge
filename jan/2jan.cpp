5e236f60d1cc2bbaa2c5a367c20a9cbda37b6762
Jaideep Goyal <m00758942@gmail.com>
2026-01-02 07:58:13 +0530

    Update 2jan.cpp

---
+++ jan/2jan.cpp
@@ -0,0 +1,24 @@
+//961. N-Repeated Element in Size 2N Array
+/*
+You are given an integer array nums with the following properties:
+    nums.length == 2 * n.
+    nums contains n + 1 unique elements.
+    Exactly one element of nums is repeated n times.
+Return the element that is repeated n times.
+*/
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        for (auto it : mp) {
+            if (it.second > 1) {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
